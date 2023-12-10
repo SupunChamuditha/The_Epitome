511c86b6005d0a9649e7201ba6e18d56efd6975e
P P SENEVIRATHNA <pasiya20024@gmail.com>
2023-12-10 23:32:34 +0530

    Modified Home.jsx(import Footer)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React, { Fragment } from "react";
 import Header from "../components/header/Header";
+import Footer from "../components/footer/Footer";
 
 const Home = () => {
   return (
     <Fragment>
       <Header />
+      <Footer/>
     </Fragment>
   );
 };
