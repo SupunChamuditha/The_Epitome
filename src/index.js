55e501e74e07cb8122371338e911bf0296a84d07
P P SENEVIRATHNA <pasiya20024@gmail.com>
2023-12-10 12:08:17 +0530

    Modified index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom";
 
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
-
+import "remixicon/fonts/remixicon.css";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
 
 import App from "./App";
 
