7f6562429fff7a1bee0797a08139e223bd3e3de7
P P SENEVIRATHNA <pasiya20024@gmail.com>
2023-12-10 16:51:25 +0530

    modified app.css(change background colour)

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,10 +1,84 @@
+import React, { useRef } from "react";
+import "./header.css";
 
-import React from 'react'
-import './header.css';
-const header = () => {
+import { Container } from "reactstrap";
+
+const navLinks = [
+  {
+    display: "Home",
+    url: "#",
+  },
+  {
+    display: "About",
+    url: "#",
+  },
+  {
+    display: "Menu",
+    url: "#",
+  },
+  {
+    display: "Recipes",
+    url: "#",
+  },
+  {
+    display: "Contact",
+    url: "#",
+  },
+];
+
+const Header = () => {
+  const menuRef = useRef();
+
+  const menuToggle = () => menuRef.current.classList.toggle("active__menu");
   return (
-    <div>header</div>
-  )
-}
+    <header className="header">
+      <Container>
+        <div className="navigation">
+          <div className="logo">
+            <h2 className=" d-flex align-items-center gap-1">
+              <span>
+                <i class="ri-restaurant-2-line"></i>
+              </span>{" "}
+              Chef Food
+            </h2>
+          </div>
 
-export default header
+          <div className="nav__menu" ref={menuRef}>
+            <div className="nav__list__wrapper d-flex align-items-center gap-5">
+              <ul className="nav__list">
+                {navLinks.map((item, index) => (
+                  <li className="nav__item" key={index}>
+                    <a href={item.url} onClick={menuToggle}>
+                      {item.display}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+
+          <div className="sign">
+            <div>
+              <a className="btn-signin" href="#">
+                Sign in
+              </a>
+            </div>
+            <div>
+              <a className="btn-signup" href="#">
+                Sign up
+              </a>
+            </div>
+          </div>
+
+          <div className="mobile__menu" onClick={menuToggle}>
+            <span>
+              <i className="ri-menu-line"></i>
+            </span>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
