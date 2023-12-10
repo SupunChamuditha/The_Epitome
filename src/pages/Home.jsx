03cb7907f80e10481423a3de521aee9caa0af6cb
P P SENEVIRATHNA <pasiya20024@gmail.com>
2023-12-10 10:07:50 +0530

    modyfied Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Header from "../components/header/header";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
 };
 
 export default Home;
